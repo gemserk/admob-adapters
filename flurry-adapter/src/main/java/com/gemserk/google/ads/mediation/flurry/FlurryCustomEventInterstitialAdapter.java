9188c349d920ac53e3a732684c2f211def1e021c
Ariel Coppes <ariel.coppes@gemserk.com>
2012-09-13 14:27:37 -0300

    started to add an implementation to the CustomEventInterstitialAdapter for Flurry

--- flurry-adapter/src/main/java/com/gemserk/google/ads/mediation/flurry/FlurryCustomEventInterstitialAdapter.java
+++ flurry-adapter/src/main/java/com/gemserk/google/ads/mediation/flurry/FlurryCustomEventInterstitialAdapter.java
@@ -1,13 +1,57 @@
 package com.gemserk.google.ads.mediation.flurry;
 
-import android.app.Activity;
+import java.util.Map;
 
+import android.app.Activity;
+import android.util.Log;
+
+import com.flurry.android.FlurryAdSize;
+import com.flurry.android.FlurryAdType;
 import com.flurry.android.FlurryAgent;
+import com.flurry.android.IListener;
 import com.google.ads.mediation.MediationAdRequest;
 import com.google.ads.mediation.customevent.CustomEventInterstitial;
 import com.google.ads.mediation.customevent.CustomEventInterstitialListener;
 
 public class FlurryCustomEventInterstitialAdapter implements CustomEventInterstitial {
+
+	private class FlurryInterstitialAdListener implements IListener {
+
+		private CustomEventInterstitialListener listener;
+
+		public FlurryInterstitialAdListener(CustomEventInterstitialListener listener) {
+			this.listener = listener;
+		}
+
+		@Override
+		public void onAdClosed(String arg0) {
+			Log.d(FlurryLogTag.Tag, "On ad closed " + arg0);
+			listener.onDismissScreen();
+		}
+
+		@Override
+		public void onApplicationExit(String arg0) {
+			Log.d(FlurryLogTag.Tag, "On application exit " + arg0);
+			listener.onLeaveApplication();
+		}
+
+		@Override
+		public void onRenderFailed(String arg0) {
+			Log.d(FlurryLogTag.Tag, "On render failed " + arg0);
+			listener.onFailedToReceiveAd();
+		}
+
+		@Override
+		public void onReward(String arg0, Map<String, String> arg1) {
+			Log.d(FlurryLogTag.Tag, "On reward " + arg0);
+		}
+
+		@Override
+		public boolean shouldDisplayAd(String arg0, FlurryAdType flurryAdType) {
+			return flurryAdType == FlurryAdType.WEB_BANNER;
+		}
+
+	}
 
 	@Override
 	public void requestInterstitialAd(CustomEventInterstitialListener listener, Activity activity, String label, //
@@ -15,11 +59,16 @@ public class FlurryCustomEventInterstitialAdapter implements CustomEventIntersti
 		FlurryAgent.initializeAds(activity);
 		FlurryAgent.enableTestAds(mediationAdRequest.isTesting());
 
+		String adSpace = serverParameter;
+		
+		FlurryAgent.setAdListener(new FlurryInterstitialAdListener(listener));
+		
+		FlurryAgent.isAdAvailable(activity, adSpace, FlurryAdSize.FULLSCREEN, 10000L);
 	}
 
 	@Override
 	public void showInterstitial() {
-
+		
 	}
 
 }
