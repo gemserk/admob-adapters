2fe22b3532c197139f8371a7141a0bfa26a58a28
Ariel Coppes <ariel.coppes@gemserk.com>
2012-12-03 13:58:18 -0200

    changed TapJoyCustomBannner to use the adView.setInitialScale(scale) assuming the adView is a WebView (which we know based on the code of Tapjoy), right now if we are on 320 or less we ask for a 320x50 ad and scale it down to match the corresponding ad width for the device, otherwise we ask always for the 640x100 and scale down or up to match the corresponding ad width for the device

--- tapjoy-adapter/src/main/java/com/gemserk/google/ads/mediation/tapjoy/TapJoyCustomEventBanner.java
+++ tapjoy-adapter/src/main/java/com/gemserk/google/ads/mediation/tapjoy/TapJoyCustomEventBanner.java
@@ -1,8 +1,10 @@
 package com.gemserk.google.ads.mediation.tapjoy;
 
 import android.app.Activity;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
 
 import com.google.ads.AdSize;
 import com.google.ads.mediation.MediationAdRequest;
@@ -10,37 +12,61 @@ import com.google.ads.mediation.customevent.CustomEventBanner;
 import com.google.ads.mediation.customevent.CustomEventBannerListener;
 import com.tapjoy.TapjoyConnect;
 import com.tapjoy.TapjoyDisplayAdNotifier;
+import com.tapjoy.TapjoyDisplayAdSize;
 
 public class TapJoyCustomEventBanner implements CustomEventBanner {
 
-	private static final String TapJoyBannerTag = "TapJoyBanner";
+	private static final String TAG = "TapJoyCustomEventBanner";
+	private int tapJoyAdScale;
 
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
-
+		Log.d(TAG, "destroy()");
 	}
 
 	@Override
-	public void requestBannerAd(final CustomEventBannerListener listener, Activity activity, String label, String serverParameter, AdSize size, MediationAdRequest mediationAdRequest, Object customEventExtra) {
+	public void requestBannerAd(final CustomEventBannerListener listener, final Activity activity, String label, String serverParameter, final AdSize size, MediationAdRequest mediationAdRequest, Object customEventExtra) {
+		Log.i(TAG, "Received a banner ad request for " + label + " with size " + size);
 
-		Log.i(TapJoyBannerTag, "Received a banner ad request for " + label);
+		DisplayMetrics metrics = new DisplayMetrics();
+		activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
-		TapjoyConnect.getTapjoyConnectInstance().setDisplayAdSize(size.getWidth() + "x" + size.getHeight());
-		TapjoyConnect.getTapjoyConnectInstance().getDisplayAdWithCurrencyID(serverParameter,new TapjoyDisplayAdNotifier() {
-			
+		int width = (int) (size.getWidth() * metrics.density);
+
+		int tapJoyAdWidth = 320;
+
+		String tapJoyAdSize = TapjoyDisplayAdSize.TJC_DISPLAY_AD_SIZE_320X50;
+		if (width > 320) {
+			tapJoyAdSize = TapjoyDisplayAdSize.TJC_DISPLAY_AD_SIZE_640X100;
+			tapJoyAdWidth = 640;
+		}
+
+		tapJoyAdScale = Math.round(100 * ((float) width) / ((float) tapJoyAdWidth));
+
+		Log.i(TAG, "Ad width:" + width + ", tapJoyAdWidth: " + tapJoyAdWidth + ", scale: " + tapJoyAdScale);
+
+		TapjoyConnect tapjoy = TapjoyConnect.getTapjoyConnectInstance();
+
+		tapjoy.enableDisplayAdAutoRefresh(false);
+
+		tapjoy.setDisplayAdSize(tapJoyAdSize);
+		tapjoy.getDisplayAdWithCurrencyID(serverParameter, new TapjoyDisplayAdNotifier() {
+
 			@Override
 			public void getDisplayAdResponseFailed(String paramString) {
-				Log.i(TapJoyBannerTag, "Banner Response Failed - " + paramString);
+				Log.i(TAG, "Banner Response Failed - " + paramString);
 				listener.onFailedToReceiveAd();
 			}
-			
+
 			@Override
-			public void getDisplayAdResponse(View view) {
-				Log.i(TapJoyBannerTag, "Banner Response succeded");
-				listener.onReceivedAd(view);
+			public void getDisplayAdResponse(View adView) {
+				Log.i(TAG, "Banner Response succeded");
+
+				((WebView) adView).setInitialScale(tapJoyAdScale);
+
+				listener.onReceivedAd(adView);
 			}
-		}); 
+		});
 	}
 
 }
